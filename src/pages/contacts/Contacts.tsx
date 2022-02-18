cf41252d0acd49972dc3a78577ead186134c5d7c
Brenda <contatobrendapaiva@outlook.com>
2022-02-18 17:20:08 -0300

    Atualizações do Projeto

--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Grid, Box, Typography, Button} from '@material-ui/core';
+import {Grid, Box, Typography, Button, TextField,} from '@material-ui/core';
 import './Contacts.css';
 import { Link } from 'react-router-dom';
 
@@ -8,23 +8,24 @@ function Contacts() {
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#a7f4fa" }}>
-                <Grid alignItems="center" item xs={6}>
-                    <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a).</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Conectando caminhos e orientando para o futuro.</Typography>
-                    </Box>
-                    <Box display="flex" justifyContent="center">
-                        <Box marginRight={1}>
-                        <Link to="/contacts" className="text-decorator-none"></Link>
+            <Grid xs={6} alignItems="center">
+                <Box paddingX={20}>
+                    <form>
+                        <Typography className="texts" variant="h3" gutterBottom color="textPrimary" component="h3" align="center">Entre em contato</Typography>
+                        <TextField id="nome" label="nome" variant="outlined" name="nome" margin="normal" fullWidth />
+                        <TextField id="assunto" label="assunto" variant="outlined" name="assunto" margin="normal" type="password" fullWidth />
+                        <Box marginTop={2} textAlign="center">
+                            <Link to="/home" className="text-decorator-none">
+                                <Button className="button-style" type="submit" variant="contained" color="primary">
+                                    Enviar
+                                </Button>
+                            </Link>
                         </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </form>
+                    <Box display="flex" justifyContent="center" marginTop={7}>
                     </Box>
-                </Grid>
-                <Grid item xs={6} >
-                    <img src="https://imgur.com/jOBJ0DL.png" alt="" width="685px" height="479px" />
-                </Grid>
-                <Grid xs={12} style={{ backgroundColor: "white" }}>
-                </Grid>
+                </Box>
+            </Grid>
             </Grid>
         </>
     )
