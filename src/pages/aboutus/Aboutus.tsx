fd1b503f3862e36049d849e8ee1c472819dc99b6
Brenda <contatobrendapaiva@outlook.com>
2022-02-18 13:53:37 -0300

    "Atualizações de Projeto"

--- src/pages/aboutus/Aboutus.tsx
+++ src/pages/aboutus/Aboutus.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import {Grid, Typography, Box, Button} from '@material-ui/core';
 import './Aboutus.css';
+import { Link } from 'react-router-dom';
+
 
 function Aboutus() {
     return (
         <>
-             
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#2191D1" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a).</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Conectando caminhos e orientando para o futuro.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        <Link to="/aboutus" className="text-decorator-none"> </Link>                    
+                    </Box>       
+
+                    
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://imgur.com/jOBJ0DL.png" alt="" width="685px" height="479px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     )
 }
--- src/pages/contacts/Contacts.tsx
+++ src/pages/contacts/Contacts.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import {Grid, Box, Typography, Button} from '@material-ui/core';
 import './Contacts.css';
+import { Link } from 'react-router-dom';
+
 
 function Contacts() {
     return (
         <>
-           
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#a7f4fa" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a).</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Conectando caminhos e orientando para o futuro.</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        <Link to="/contacts" className="text-decorator-none"></Link>
+                                
+                            
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://imgur.com/jOBJ0DL.png" alt="" width="685px" height="479px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     )
 }
