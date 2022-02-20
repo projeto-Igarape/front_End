2828a32ae88f6d2e7a88e765582d33c360df81b0
Brenda <contatobrendapaiva@outlook.com>
2022-02-20 15:49:31 -0300

    Atualizações NavBar

--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box} from '@material-ui/core';
+import { AppBar, Grid, Toolbar, Typography, Box, BottomNavigationAction} from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 function Navbar(){
     return (
-        <>
-        
-        <AppBar position="relative">
+        <>  
+        <AppBar position="relative" style={{ backgroundColor: "#2191D1" }}>
+            <Grid item xs={12} >
+                    <img src="https://imgur.com/G9gxl19.png" alt="" width="1349px" height="67px"/>
+            </Grid>
                 <Toolbar variant="dense">
                     <Box style={{ cursor: "pointer" }}>
-                        <Typography variant="h5" color="inherit">
+                        <Typography variant="h5" color="inherit" >
                             Projeto Igarapé
                         </Typography>
                     </Box>
