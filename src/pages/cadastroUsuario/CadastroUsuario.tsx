183ac78fd93d85603b4c0810ac8acd8b0e1f9170
Brenda <contatobrendapaiva@outlook.com>
2022-02-23 11:34:27 -0300

    Cadastro Usuario

--- src/pages/cadastroUsuario/CadastroUsuario.tsx
+++ src/pages/cadastroUsuario/CadastroUsuario.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './CadastroUsuario.css';
 
-function CadastroUsuario(){
+function CadastroUsuario() {
 
-    return(
-        <h1>Cadastro Usuario</h1>
+    return (
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid item xs={6} className='imagem2'></Grid>
+            <Grid item xs={6} alignItems='center'>
+                <Box paddingX={10}>
+                    <form>
+                        <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos2'>Cadastrar</Typography>
+                        <TextField id='nome' label='Nome' variant='outlined' name='nome' margin='normal' fullWidth />
+                        <TextField id='usuario' label='Usuário(a)' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField id='senha' label='Senha' variant='outlined' name='senha' margin='normal' type='password' fullWidth />
+                        <TextField id='confirmarSenha' label='confirmar Senha' variant='outlined' name='confirmarSenha' margin='normal' type='password' fullWidth />
+                        <Box marginTop={2} textAlign='center'>
+                            <Link to='/login' className='text-decorator-none'>
+                                <Button type='submit' variant='contained' color='primary'>
+                                    Cadastrar
+                                </Button>
+                            </Link>
+                            <Button variant='contained' color='secondary' className='btnCancelar'>
+                                Cancelar
+                            </Button>
+                        </Box>
+                    </form>
+                </Box>
+            </Grid>
+        </Grid>
     )
 }
 
