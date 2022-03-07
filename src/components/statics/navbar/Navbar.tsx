cc081932bb9d6f0074e9506d78228b95a2088e6a
Brenda <contatobrendapaiva@outlook.com>
2022-03-07 12:40:17 -0300

    Atualizações

--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -1,77 +1,51 @@
 import React from 'react';
-import { Box, } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
 import './Navbar.css'
-import { useDispatch, useSelector } from 'react-redux';
-import { addToken } from '../../../store/tokens/actions';
-import { TokenState } from '../../../store/tokens/tokensReducer';
-import { toast } from 'react-toastify';
-
-
 function Navbar() {
-    const token = useSelector<TokenState, TokenState["tokens"]>(
-        (state) => state.tokens
-    );
-    let history = useHistory();
-    const dispatch = useDispatch();
-
-    function goLogout() {
-        dispatch(addToken(''));
-        toast.info('Usuário(a) deslogado', {
-            position: "top-right",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: false,
-            draggable: false,
-            theme: "colored",
-            progress: undefined,
-
-        });
-        history.push('/login')
-    }
-
-    var navbarComponent;
-
-    if (token != "") {
-        navbarComponent = <header id="header">
-            <a id='logo' href="">Logo</a>
-            <nav id="nav">
-                <button id="btn-mobile">Projeto Igarapé</button>
-                <ul id="menu">
-                    <Link to='/home'>
-                        <Box mx={1}>
-                            <li><a href="/">Home</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/aboutus'>
-                        <Box mx={1}>
-                            <li><a href="/">Sobre nós</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/'>
-                        <Box mx={1} >
-                            <li><a href="/">Cisterna</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/contacts'>
-                        <Box mx={1} >
-                            <li><a href="/">Contato</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/login'>
-                        <Box mx={1}  onClick={goLogout}>
-                            <li><a href="/">Logout</a></li>
-                        </Box>
-                    </Link>
-                </ul>
-            </nav>
-        </header>
-    }
     return (
         <>
-            {navbarComponent}
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            Projeto Igarapé
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Sobre-nós
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Cisternas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                Contato
+                            </Typography>
+                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    Logout
+                                </Typography>
+                            </Box>
+                        </Link>
+                        
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
