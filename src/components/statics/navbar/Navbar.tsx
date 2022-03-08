7f633d1dbe65c305bc4ca4666fefff7e70a55f8f
Brenda <contatobrendapaiva@outlook.com>
2022-03-08 01:15:09 -0300

    Navbar Atualizações

--- src/components/statics/navbar/Navbar.tsx
+++ src/components/statics/navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Box, } from '@material-ui/core';
+import { Box, Toolbar, AppBar } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
 import './Navbar.css'
 import useLocalStorage from 'react-use-localstorage';
+import { Avatar } from '@mui/material';
 
 
 function Navbar() {
@@ -11,7 +12,7 @@ function Navbar() {
     const [token, setToken] = useLocalStorage('token');
     let history = useHistory();
 
-    function goLogout(){
+    function goLogout() {
         setToken('')
         alert('Usuário deslogado!')
         history.push('/login')
@@ -19,49 +20,54 @@ function Navbar() {
 
     return (
         <>
-           <header id="header">
-            <a id='logo' href="">Logo</a>
-            <nav id="nav">
-                <button id="btn-mobile">Projeto Igarapé</button>
-                <ul id="menu">
-                    <Link to='/home'>
-                        <Box mx={1}>
-                            <li><a href="/">Home</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/aboutus'>
-                        <Box mx={1}>
-                            <li><a href="/">Sobre Nós</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/'>
-                        <Box mx={1} >
-                            <li><a href="/">Produtos</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/temas'>
-                        <Box mx={1} >
-                            <li><a href="/">Cisterna</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/contacts'>
-                        <Box mx={1} >
-                            <li><a href="/">Contato</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/cadastroCategoria'>
-                        <Box mx={1} >
-                            <li><a href="/">Cadastrar Categoria</a></li>
-                        </Box>
-                    </Link>
-                    <Link to='/login'>
-                        <Box mx={1}  onClick={goLogout}>
-                            <li><a href="/">Logout</a></li>
-                        </Box>
-                    </Link>
-                </ul>
-            </nav>
-        </header>
+        <AppBar color="inherit">
+            <header id="header" className="container-cabecalho">
+                <Toolbar>
+                    <img src="https://imgur.com/KdBVSES.png.png" alt="Projeto Igarapé Logo" className="imagem-logo" />
+                    </Toolbar>
+                    <nav id="nav" className="nav-links-cabecalho">
+                        <button id="btn-mobile">Projeto Igarapé</button>
+                        <ul id="menu">
+                            <Link to='/home'>
+                                <Box mx={1}>
+                                    <li><a href="/">Home</a></li>
+                                </Box>
+                            </Link>
+                            <Link to='/cadastroCategoria'>
+                                <Box mx={1} >
+                                    <li><a href="/">Cadastrar Categoria</a></li>
+                                </Box>
+                            </Link>
+                            <Link to='/'>
+                                <Box mx={1} >
+                                    <li><a href="/">Produtos</a></li>
+                                </Box>
+                            </Link>
+                            <Link to='/temas'>
+                                <Box mx={1} >
+                                    <li><a href="/">Cisterna</a></li>
+                                </Box>
+                            </Link>
+                            
+                            <Link to='/aboutus'>
+                                <Box mx={1}>
+                                    <li><a href="/">Sobre Nós</a></li>
+                                </Box>
+                            </Link>
+                            <Link to='/contacts'>
+                                <Box mx={1} >
+                                    <li><a href="/">Contato</a></li>
+                                </Box>
+                            </Link>
+                            <Link to='/login'>
+                                <Box mx={1} onClick={goLogout}>
+                                    <li><a href="/">Logout</a></li>
+                                </Box>
+                            </Link>
+                        </ul>
+                    </nav>
+            </header>
+            </AppBar>
         </>
     )
 }
