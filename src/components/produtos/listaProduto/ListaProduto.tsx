72702b445b25ed879f35324a06648fa6c0e20f5e
thatipiresss <thaticmg@gmail.com>
2022-03-08 13:08:51 -0300

    Implementando lista produto

---
+++ src/components/produtos/listaProduto/ListaProduto.tsx
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Produto from '../../../models/Produto';
+import { busca } from '../../../services/Service'
+import { Box, Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+import './ListaProduto.css';
+import useLocalStorage from 'react-use-localstorage';
+import { useHistory } from 'react-router-dom'
+
+function ListaProduto() {
+  const [produto, setProduto] = useState<Produto[]>([])
+  const [token, setToken] = useLocalStorage('token');
+  let history = useHistory();
+
+  useEffect(() => {
+    if (token == "") {
+      alert("Você precisa estar logado")
+      history.push("/login")
+
+    }
+  }, [token])
+
+  async function getProduto() {
+    await busca("/produtos", setProduto, {
+      headers: {
+        'Authorization': token
+      }
+    })
+  }
+
+  useEffect(() => {
+
+    getProduto()
+
+  }, [produto.length])
+
+  return (
+    <>
+      {
+        produto.map(produto => (
+          <Box m={2} >
+            <Card variant="outlined">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Produtos
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  {produto.nome}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {produto.preco}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {produto.foto}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {produto.categoria}
+                  </Typography>
+              </CardContent>
+              <CardActions>
+                <Box display="flex" justifyContent="center" mb={1.5}>
+
+                  <Link to={`/listaProduto/${produto.id}`} className="text-decorator-none" >
+                    <Box mx={1}>
+                      <Button variant="contained" className="marginLeft" size='small' color="primary" >
+                        atualizar
+                      </Button>
+                    </Box>
+                  </Link>
+                  <Link to={`/deletarPostagem/${produto.id}`} className="text-decorator-none">
+                    <Box mx={1}>
+                      <Button variant="contained" size='small' color="secondary">
+                        deletar
+                      </Button>
+                    </Box>
+                  </Link>
+                </Box>
+              </CardActions>
+            </Card>
+          </Box>
+        ))
+      }
+    </>
+  )
+}
+
+export default ListaProduto;
