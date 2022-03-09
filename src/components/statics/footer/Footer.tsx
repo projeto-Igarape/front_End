6addd07cac077531af26202cc9d08f1669ae74e0
thatipiresss <thaticmg@gmail.com>
2022-03-09 13:21:25 -0300

    Estilizando footer

--- src/components/statics/footer/Footer.tsx
+++ src/components/statics/footer/Footer.tsx
@@ -1,23 +1,68 @@
 import React from 'react';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import {Grid, Box, Typography} from '@material-ui/core';
 import './Footer.css'
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
 
 function Footer(){
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state)=> state.tokens
+    );
     return (
         <>
-            <Grid container direction="row" justifyContent="center" alignItems="center" className='footer' >
-                    <Grid alignItems="center" item xs={12}>
-                        <Box className='box'>
+                <footer className='footer'>
+                <Box className='box'>
+                
+                                <Box display="flex" alignItems="left" justifyContent="left">
+                                   <a href="http://localhost:3000/home">
+                                    <img src="https://imgur.com/KdBVSES.png.png" alt="Projeto Igarapé Logo" className="img-logo" />
+                                    </a>
+                                </Box>
+                                                 
+                            </Box>
                             <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center" className='font'>
-                                <Typography variant="h5" align="center" gutterBottom>Siga-nos nas redes sociais </Typography>
-                            </Box>
-                            <Box display="flex" alignItems="center" justifyContent="center">
-                                <a href="https://github.com/ProjetoIgarape">
-                                    <GitHubIcon className='font' />
-                                </a>
-                            </Box>
-                        </Box>
+                                <Typography variant="h5" align="center" gutterBottom>Projeto Igarapé </Typography>                            
+                                </Box>    
+                 <div className='container'>
+                     
+                             <div className='footer-col'>
+                            <h4>aba1</h4>                    
+                            <ul>
+                                <li><a href='testinho'>teste</a></li>
+                                <li><a href='testinho'>teste</a></li>
+                                <li><a href='testinho'>teste</a></li>
+                                <li><a href='testinho'>teste</a></li>
+                            </ul>
+                         </div>
+                     
+                     
+                     <div className='footer-col'>
+                         <h4>aba2</h4>
+                            <ul>
+                                <li><a href='testinho2'>teste</a></li>
+                                <li><a href='testinho2'>teste</a></li>
+                                <li><a href='testinho2'>teste</a></li>
+                                <li><a href='testinho2'>teste</a></li>
+                            </ul>
+
+                     </div>
+                     
+                    <div className='footer-col'>
+                        <h4>aba3</h4> 
+                            <div className='social-links'>
+                                <a href ='https://www.facebook.com/thatipiress/'><FacebookIcon /></a>
+                                <a href="https://instagram.com/thatipiresss"><InstagramIcon/></a>
+                                <a href="https://www.linkedin.com/in/thatipiresss/"><LinkedInIcon /></a>
+                                <a href="https://github.com/thatipiresss"><GitHubIcon/></a>
+                            </div>
+                    </div>
+                              </div>
+                    
                         <Box className='box1'>
                             <Box paddingTop={3} className='font'>
                                 <Typography variant="subtitle2" align="center" gutterBottom>© 2022 Copyright:</Typography>
@@ -28,8 +73,8 @@ function Footer(){
                                 </a>
                             </Box>
                         </Box>
-                    </Grid>
-                </Grid>
+                    
+                </footer>
         </>
     )
 }
