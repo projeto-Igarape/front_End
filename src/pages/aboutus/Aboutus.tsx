5f15c528e45d2f58267cd76694a629a2382bec5b
Rafael <rafaelferreira.0096@gmail.com>
2022-03-10 13:40:32 -0300

    css sobre nós

--- src/pages/aboutus/Aboutus.tsx
+++ src/pages/aboutus/Aboutus.tsx
@@ -39,39 +39,46 @@ function Aboutus() {
                         <Typography className='titulosobre' variant="h5" gutterBottom color="textPrimary" component="h5" align="center" >
                             
                             <p>
-                            O Projeto Igarapé nasceu na Generation Brasil, onde, com base nos Objetivos de Desenvolvimento Sustentável sobre Água Potável e saneamento, temos como principal 
-                            intuito  melhorar a disponibilização de água potável para consumo e saneamento básico em comunidades de baixa renda, além de conectar para orientar sobre a gestão 
-                            sustentável da água potável e do saneamento para todos.
+                            O Projeto Igarapé nasceu na Generation Brasil onde, com base nos Objetivos de 
+                            Desenvolvimento Sustentável sobre Água Potável e Saneamento, temos como principal 
+                            intuito melhorar a disponibilização de água potável para consumo e saneamento básico 
+                            em comunidades de baixa renda, além de conectar para orientar sobre a gestão 
+                            sustentável da água potável e do saneamento para todos. Acreditamos que a escassez de 
+                            água potável e a falta de saneamento básico, traz riscos iminentes à saúde, uma vez que é 
+                            um recurso natural indispensável e essencial para a vida.
                             </p>
-                            <br/>
+                            <br />
                             <p>
-                            Acreditamos que a escassez de água potável e a falta de saneamento básico, traz riscos iminentes à saúde, uma vez que é um recurso natural indispensável e essencial 
-                            para a vida.
+                            Muitas comunidades de baixa renda não têm acesso ao mínimo do saneamento básico e/ou água potável
+                            e própria para consumo (cerca de 35 milhões de brasileiros). 
+                            Pessoas ficam mais doentes do que o normal por não terem acesso 
+                            a um recurso natural indispensável e essencial. 
+                            </p> 
+                            <br />
+                            <p>
+                            Na pandemia, a situação se agravou, já que, de acordo com a Organização Mundial da Saúde (OMS), 
+                            a lavagem completa das mãos está entre as medidas mais eficazes 
+                            para limitar a propagação da Covid-19.
+                            <p>
+                            <br />
                             </p>
-                            <br/>
+                            Contamos com essa iniciativa para arrecadar valores que serão revertidos na compra de
+                            cisternas, possibilitando a coleta e tratamento da água da chuva e reutilização de água 
+                            armazenada nas mesmas.
                             <p>
-                            Muitas comunidades de baixa renda não têm acesso ao mínimo do saneamento básico e/ou água potável e própria para consumo (Cerca de 35 milhões de brasileiros). 
-                            Pessoas ficam mais doentes do que o normal por não terem acesso a um recurso natural indispensável e essencial.  
+                            <br />
                             </p>
-                            <br/>
-                            <p>
-                            Na pandemia, a situação se agravou ainda mais, já que, de acordo com a Organização Mundial da Saúde (OMS), a lavagem completa das mãos está entre as medidas mais eficazes 
-                            para limitar a propagação da covid-19.
+                            Inicialmente implantaremos esse sistema de captação de água, na comunidade da Tribo, 
+                            localizada no bairro Jardim Damasceno, Zona Norte de São Paulo. A favela fica em uma 
+                            pequena parte ocupada de uma área de 461 mil m² de terreno acidentado e íngreme às 
+                            margens da serra da Cantareira. Vivem ali cerca de mil famílias. 
                             </p>
-                            <br/>
+                            <br />
                             <p>
-                            Contamos com essa iniciativa para arrecadar valores que serão revertidos na compra de cisternas, possibilitando a coleta e tratamento da água da chuva e reutilização de água 
-                            armazenada nelas.
-                            </p>
-                            <br/>
-                            <p>
-                            Inicialmente implantaremos esse sistema de captação de água, na comunidade da Tribo, localizada no bairro Jardim Damasceno, Zona Norte de São Paulo. A favela fica em uma 
-                            pequena parte ocupada de uma área de 461 mil m² de terreno acidentado e íngreme às margens da serra da Cantareira. Vivem ali cerca de mil famílias. 
-                            </p>
-                            <br/>
-                            <p>
-                            As famílias beneficiadas são capacitadas pelo próprio Projeto, através de um cadastro, onde a partir do mesmo conseguimos contatá-las. Assim o processo de implementação das 
-                            tecnologias é realizado em regime de cooperação, gerando sentimento de pertencimento, o que promove maior sustentabilidade ao equipamento instalado. 
+                            As famílias beneficiadas são capacitadas pelo próprio Projeto, através de um cadastro, 
+                            onde a partir do mesmo conseguimos contatá-las. Assim o processo de implementação das 
+                            tecnologias é realizado em regime de cooperação, gerando sentimento de pertencimento, 
+                            o que promove maior sustentabilidade ao equipamento instalado. 
                             </p>
                             
                         </Typography>
@@ -84,8 +91,8 @@ function Aboutus() {
                     <Box marginRight={1}>
                             <Typography className='saibamais1' variant='subtitle1' gutterBottom align='center'> Quer saber mais sobre o projeto?</Typography>
                         </Box>
-                        <Link to='/cisterna'>
-                        <Typography variant='subtitle1' gutterBottom align='center' className="clique1">Clique aqui!</Typography>
+                        <Link className="clique" to='/cisterna'>
+                        <Typography variant='subtitle1' gutterBottom align='center' className="saibamais1">Clique aqui!</Typography>
                         </Link>
                     </Box>
                 </Grid>
