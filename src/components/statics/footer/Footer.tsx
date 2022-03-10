cc767574e1b87e34339b2d7b1154b455fcf05847
thatipiresss <thaticmg@gmail.com>
2022-03-10 10:16:51 -0300

    ajustando footer

--- src/components/statics/footer/Footer.tsx
+++ src/components/statics/footer/Footer.tsx
@@ -36,29 +36,33 @@ function Footer(){
                  <div className='container_footer'>
                      
                              <div className='footer-col'>
-                            <h4>aba1</h4>                    
+                            <h4>O que somos?</h4>                    
                             <ul>
-                                <li><a href='testinho'>teste</a></li>
-                                <li><a href='testinho'>teste</a></li>
-                                <li><a href='testinho'>teste</a></li>
-                                <li><a href='testinho'>teste</a></li>
+                                <li><a href='/listaProduto'>Produtos</a></li>
+                                <li><a href='/aboutus'>Sobre Nós</a></li>
+                                <li><a href='/parceiros'>Parceiros</a></li>
+                                <li><a href='/cisterna'>Cisterna</a></li>
+
+                                                    
                             </ul>
                          </div>
                      
                      
                      <div className='footer-col'>
-                         <h4>aba2</h4>
+                         <h4>Fale conosco</h4>
                             <ul>
-                                <li><a href='testinho2'>teste</a></li>
-                                <li><a href='testinho2'>teste</a></li>
-                                <li><a href='testinho2'>teste</a></li>
-                                <li><a href='testinho2'>teste</a></li>
+                                <li><a href='/contacts'>Contato</a></li>
+                                <li><a href='/contacts'>Seja um parceiro</a></li>
+                                <li><a href='/contacts'>Trabalhe Conosco</a></li>
+                                <li><a href='/teste'>Termos&Condições</a></li>
+                                
+                                
                             </ul>
 
                      </div>
                      
                     <div className='footer-col'>
-                        <h4>aba3</h4> 
+                        <h4>Nossas redes</h4> 
                             <div className='social-links'>
                                 <a href ='https://www.facebook.com/thatipiress/'><FacebookIcon /></a>
                                 <a href="https://instagram.com/thatipiresss"><InstagramIcon/></a>
@@ -71,9 +75,14 @@ function Footer(){
                     
                         <Box className='box1'>
                             <Box paddingTop={3} className='font'>
-                                <Typography variant="subtitle2" align="center" gutterBottom>© 2022 Copyright:</Typography>
+                                <Typography variant="subtitle2" align="center" gutterBottom>© 2022 Copyright</Typography>
                             </Box>
-                            <Box className='font'>
+
+                            <Box paddingTop={0} className='font'>
+                                <Typography variant="subtitle2" align="center" gutterBottom> Projeto Igarapé Brasil LTDA. Todos os direitos reservados. Avenida Paulista, 2439
+São Paulo - SP</Typography>
+                            </Box>
+                                                        <Box className='font'>
                                 <a href="https://brazil.generation.org/">
                                     <Typography variant="subtitle2" gutterBottom align="center">brasil.generation.org</Typography>
                                 </a>
