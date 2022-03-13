313a7578080d379ad38127c26de61cc2fd87e044
Brenda <contatobrendapaiva@outlook.com>
2022-03-13 18:40:31 -0300

    Atualizações

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -43,15 +43,14 @@ function Home(){
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" className='home'>
-                <Grid alignItems="center" item xs={6}>
+                <Grid alignItems="center" item xs={12}>
                     <Box paddingX={20} >
-                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a).</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Conectando caminhos e orientando para o futuro.</Typography>
+                       
                     </Box>
                    
                    {/* <div>
                         <Carousel breakPoints={breakPoints}>
-                        <Item><img src="" alt="" /></Item>
+                        <Item><img src="https://imgur.com/5Sp2B3x.png" alt="" /></Item>
                         
                         </Carousel>
                     </div> */}
@@ -59,11 +58,10 @@ function Home(){
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>   
                              </Box>
-                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#2191D1", color: "white" }}>Ver Produtos</Button>
                     </Box>
                 </Grid>
-                <Grid item xs={6} >
-                    <img src="https://imgur.com/5Sp2B3x.png" alt="" width="605px" height="379px" />
+                <Grid item xs={12} >
+                    <img src="https://imgur.com/5Sp2B3x.png" alt="" className="vídeo" />
                 </Grid>
                 <Grid xs={12} style={{ backgroundColor: "white" }}>
                     <TabProduto />
